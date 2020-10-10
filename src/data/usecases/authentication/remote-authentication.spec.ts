74449e92e9dc2b86ae84a05088c4bf098d7bda02
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-10 15:39:03 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -3,16 +3,18 @@ import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { mockAuthentication } from '@/domain/test/mock-authentication'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import faker from 'faker'
 import { UnexpectedError } from '@/domain/errors/unexpectd-credentials-error'
+import { AuthenticationParams } from '@/domain/usecase/authentication'
+import { AccountModel } from '@/domain/models/account-model'
+import faker from 'faker'
 
 type SystemUnderTest = {
   systemUnderTest: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
 }
 
 const makeSystemUnderTest = (url: string = faker.internet.url()): SystemUnderTest => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>()
   const systemUnderTest = new RemoteAuthentication(url, httpPostClientSpy)
   return {
     systemUnderTest,

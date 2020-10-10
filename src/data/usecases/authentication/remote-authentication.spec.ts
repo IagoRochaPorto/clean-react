d731199e8e1bc483196d27c27d6b0384869e3b03
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-10 13:45:35 -0300

    refactor: change RemoteAuthentication to use faker

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,13 @@
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
+import faker from 'faker'
 
 type SystemUnderTest = {
   systemUnderTest: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSystemUnderTest = (url: string = 'any_url'): SystemUnderTest => {
+const makeSystemUnderTest = (url: string = faker.internet.url()): SystemUnderTest => {
   const httpPostClientSpy = new HttpPostClientSpy()
   const systemUnderTest = new RemoteAuthentication(url, httpPostClientSpy)
   return {
@@ -17,7 +18,7 @@ const makeSystemUnderTest = (url: string = 'any_url'): SystemUnderTest => {
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    const url = 'other_url'
+    const url = faker.internet.url()
     const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest(url)
     await systemUnderTest.auth()
     expect(httpPostClientSpy.url).toBe(url)

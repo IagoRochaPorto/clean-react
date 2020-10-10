df2495c5a608014a347f3660dc998e8f25af28d3
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-10 13:38:09 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,11 +1,24 @@
 import { RemoteAuthentication } from './remote-authentication'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 
+type SystemUnderTest = {
+  systemUnderTest: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSystemUnderTest = (url: string = 'any_url'): SystemUnderTest => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const systemUnderTest = new RemoteAuthentication(url, httpPostClientSpy)
+  return {
+    systemUnderTest,
+    httpPostClientSpy
+  }
+}
+
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    const url = 'any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const systemUnderTest = new RemoteAuthentication(url, httpPostClientSpy)
+    const url = 'other_url'
+    const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest(url)
     await systemUnderTest.auth()
     expect(httpPostClientSpy.url).toBe(url)
   })

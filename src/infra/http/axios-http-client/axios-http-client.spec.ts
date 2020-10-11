269a16916a4240bdbd28cba688dfd1a5feed52f6
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-11 19:35:38 -0300

    test: ensure AxiosHttpClient call axios with correct URL with correct verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -9,10 +9,10 @@ const makeSystemUnderTest = (): AxiosHttpClient => {
 }
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL', async () => {
+  test('Should call axios with correct URL and verb', async () => {
     const url = faker.internet.url()
     const systemUnderTest = makeSystemUnderTest()
     await systemUnderTest.post({ url })
-    expect(mockedAxios).toHaveBeenCalledWith(url)
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
   })
 })

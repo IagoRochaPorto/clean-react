6368a9602bc7a4b17e90b8c4ea6ff2c64aa43e1c
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-12 20:48:37 -0300

    test: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,19 +1,34 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
 import Login from './login'
+import faker from 'faker'
+import { Validation } from '@/presentation/protocols/validation'
 
 type SystemUnderTestTypes = {
   systemUnderTest: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+  validate(input: object): string | null {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSystemUnderTest = (): SystemUnderTestTypes => {
-  const systemUnderTest = render(<Login />)
+  const validationSpy = new ValidationSpy()
+  const systemUnderTest = render(<Login validation={validationSpy} />)
   return {
-    systemUnderTest
+    systemUnderTest,
+    validationSpy
   }
 }
 
 describe('Login component', () => {
+  afterEach(cleanup)
   test('Should start with initial state', () => {
     const { systemUnderTest } = makeSystemUnderTest()
     const errorWrapper = systemUnderTest.getByTestId('error-wrapper')
@@ -22,12 +37,22 @@ describe('Login component', () => {
     const submitButton = systemUnderTest.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
 
-    const emailStatus = systemUnderTest.getByTestId('email')
+    const emailStatus = systemUnderTest.getByTestId('email-status')
     expect(emailStatus.title).toBe('Campo obrigatório')
     expect(emailStatus.textContent).toBe('🔴')
 
-    const passwordStatus = systemUnderTest.getByTestId('password')
+    const passwordStatus = systemUnderTest.getByTestId('password-status')
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.textContent).toBe('🔴')
+  })
+
+  test('Should call validation with correct email', () => {
+    const email = faker.internet.email()
+    const { systemUnderTest, validationSpy } = makeSystemUnderTest()
+    const emailInput = systemUnderTest.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: email } })
+    expect(validationSpy.input).toEqual({
+      email
+    })
   })
 })

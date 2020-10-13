f26c7e16b9c360d43835cd80b3e9d6e11aca4210
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-12 21:40:12 -0300

    test: ensure Login present error if password is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
 import Login from './login'
 import faker, { system } from 'faker'
-import { ValidationSpy } from '@/presentation/test'
+import { ValidationStub } from '@/presentation/test'
 
 type SystemUnderTestTypes = {
   systemUnderTest: RenderResult
-  validationSpy: ValidationSpy
+  validationStub: ValidationStub
 }
 
 const makeSystemUnderTest = (): SystemUnderTestTypes => {
-  const validationSpy = new ValidationSpy()
-  validationSpy.errorMessage = faker.random.words()
-  const systemUnderTest = render(<Login validation={validationSpy} />)
+  const validationStub = new ValidationStub()
+  validationStub.errorMessage = faker.random.words()
+  const systemUnderTest = render(<Login validation={validationStub} />)
   return {
     systemUnderTest,
-    validationSpy
+    validationStub
   }
 }
 
 describe('Login component', () => {
   afterEach(cleanup)
   test('Should start with initial state', () => {
-    const { systemUnderTest, validationSpy } = makeSystemUnderTest()
+    const { systemUnderTest, validationStub } = makeSystemUnderTest()
     const errorWrapper = systemUnderTest.getByTestId('error-wrapper')
     expect(errorWrapper.childElementCount).toBe(0)
 
@@ -30,39 +30,31 @@ describe('Login component', () => {
     expect(submitButton.disabled).toBe(true)
 
     const emailStatus = systemUnderTest.getByTestId('email-status')
-    expect(emailStatus.title).toBe(validationSpy.errorMessage)
+    expect(emailStatus.title).toBe(validationStub.errorMessage)
     expect(emailStatus.textContent).toBe('🔴')
 
     const passwordStatus = systemUnderTest.getByTestId('password-status')
-    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.title).toBe(validationStub.errorMessage)
     expect(passwordStatus.textContent).toBe('🔴')
   })
 
-  test('Should call validation with correct email', () => {
-    const email = faker.internet.email()
-    const { systemUnderTest, validationSpy } = makeSystemUnderTest()
-    const emailInput = systemUnderTest.getByTestId('email')
-    fireEvent.input(emailInput, { target: { value: email } })
-    expect(validationSpy.fieldName).toBe('email')
-    expect(validationSpy.fieldValue).toBe(email)
-  })
-
-  test('Should call validation with correct password', () => {
-    const password = faker.internet.password()
-    const { systemUnderTest, validationSpy } = makeSystemUnderTest()
-    const passwordInput = systemUnderTest.getByTestId('password')
-    fireEvent.input(passwordInput, { target: { value: password } })
-    expect(validationSpy.fieldName).toBe('password')
-    expect(validationSpy.fieldValue).toBe(password)
-  })
-
   test('Should show email error if Validation fails', () => {
-    const { systemUnderTest, validationSpy } = makeSystemUnderTest()
+    const { systemUnderTest, validationStub } = makeSystemUnderTest()
 
     const emailInput = systemUnderTest.getByTestId('email')
     fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
     const emailStatus = systemUnderTest.getByTestId('email-status')
-    expect(emailStatus.title).toBe(validationSpy.errorMessage)
+    expect(emailStatus.title).toBe(validationStub.errorMessage)
     expect(emailStatus.textContent).toBe('🔴')
+  })
+
+  test('Should show password error if Validation fails', () => {
+    const { systemUnderTest, validationStub } = makeSystemUnderTest()
+
+    const passwordInput = systemUnderTest.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const passwordStatus = systemUnderTest.getByTestId('password-status')
+    expect(passwordStatus.title).toBe(validationStub.errorMessage)
+    expect(passwordStatus.textContent).toBe('🔴')
   })
 })

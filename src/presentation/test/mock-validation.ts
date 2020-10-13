14d28d7f39d4b80f47322ba2db827a601f8afef7
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-12 21:31:59 -0300

    feat: ensure Login present error if email is invalid

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -4,7 +4,7 @@ export class ValidationSpy implements Validation {
   errorMessage: string
   fieldName: string
   fieldValue: string
-  validate(fieldName: string, fieldValue: string): string | null {
+  validate(fieldName: string, fieldValue: string): string {
     this.fieldName = fieldName
     this.fieldValue = fieldValue
     return this.errorMessage

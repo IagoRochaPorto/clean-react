5e7f9e20fbc41469b046230c0a0521c4217bd952
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-17 17:22:21 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/validation/requiredField/required-field-validation.spec.ts
+++ src/validation/requiredField/required-field-validation.spec.ts
@@ -2,15 +2,17 @@ import { RequiredFieldError } from '@/validation/errors'
 import { RequiredFieldValidation } from '@/validation/requiredField/required-field-validation'
 import faker from 'faker'
 
+const makeSystemUnderTest = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column())
+
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty ', () => {
-    const systemUnderSystem = new RequiredFieldValidation('email')
+    const systemUnderSystem = makeSystemUnderTest()
     const error = systemUnderSystem.validate('')
     expect(error).toEqual(new RequiredFieldError())
   })
 
   test('Should return falsy if field is not empty ', () => {
-    const systemUnderSystem = new RequiredFieldValidation('email')
+    const systemUnderSystem = makeSystemUnderTest()
     const error = systemUnderSystem.validate(faker.random.word())
     expect(error).toBeFalsy()
   })

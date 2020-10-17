c2f504cabf70aad8f6c94182157f642664d9db9f
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-17 17:13:09 -0300

    test: ensure requiredFieldValidation returns false if field is not empty

--- src/validation/requiredField/required-field-validation.spec.ts
+++ src/validation/requiredField/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldError } from '@/validation/errors'
 import { RequiredFieldValidation } from '@/validation/requiredField/required-field-validation'
+import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty ', () => {
     const systemUnderSystem = new RequiredFieldValidation('email')
     const error = systemUnderSystem.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is not empty ', () => {
+    const systemUnderSystem = new RequiredFieldValidation('email')
+    const error = systemUnderSystem.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })

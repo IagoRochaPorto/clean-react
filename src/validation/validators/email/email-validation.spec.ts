153036654d50f907f45e22864a178a679158b9a7
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-17 17:51:36 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,15 +2,17 @@ import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { EmailValidation } from './email-validation'
 import faker from 'faker'
 
+const makeSystemUnderTest = (): EmailValidation => new EmailValidation(faker.database.column())
+
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const systemUnderTest = new EmailValidation(faker.random.word())
+    const systemUnderTest = makeSystemUnderTest()
     const error = systemUnderTest.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falry if email is valid', () => {
-    const systemUnderTest = new EmailValidation(faker.random.word())
+    const systemUnderTest = makeSystemUnderTest()
     const error = systemUnderTest.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })

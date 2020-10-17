13a97f841a9e098253eecd0c449fdaf9e1ad6724
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-17 18:17:20 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,16 +1,19 @@
 import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { MinLengthValidation } from './min-length-validation'
+import faker from 'faker'
+
+const makeSystemUnderTest = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
-    const systemUnderTest = new MinLengthValidation('field', 5)
-    const error = systemUnderTest.validate('123')
+    const systemUnderTest = makeSystemUnderTest()
+    const error = systemUnderTest.validate(faker.random.alphaNumeric(4))
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falsy if value is valid', () => {
-    const systemUnderTest = new MinLengthValidation('field', 5)
-    const error = systemUnderTest.validate('12345')
+    const systemUnderTest = makeSystemUnderTest()
+    const error = systemUnderTest.validate(faker.random.alphaNumeric(5))
     expect(error).toBeFalsy()
   })
 })

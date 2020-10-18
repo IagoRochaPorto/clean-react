135938c3fedf201c8ee8fe0f62fceca8b5133fe6
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-18 00:07:48 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Input from './input'
 import Context from '@/presentation/contexts/form/form-context'
 
+const makeSystemUnderTest = (): RenderResult => {
+  return render(
+    <Context.Provider value={{ state: {} }}>
+      <Input name="field" />
+    </Context.Provider>
+  )
+}
+
 describe('Input Component', () => {
   test('Should begin with readOnly', () => {
-    const { getByTestId } = render(
-      <Context.Provider value={{ state: {} }}>
-        <Input name="field" />
-      </Context.Provider>
-    )
-    const input = getByTestId('field') as HTMLInputElement
+    const systemUnderTest = makeSystemUnderTest()
+    const input = systemUnderTest.getByTestId('field') as HTMLInputElement
     expect(input.readOnly).toBe(true)
   })
 })

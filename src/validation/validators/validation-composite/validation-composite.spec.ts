30c112a5a7713cc86053b60e97517ec6fb953265
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-17 21:42:47 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,14 +1,27 @@
 import { Validation } from '@/presentation/protocols/validation'
+import { FieldValidation } from '@/validation/protocols/field-validation'
 import { FieldValidationSpy } from '../test/mock-field-validation'
 import { ValidationComposite } from './validation-composite'
 
+type SystemUnderTest = {
+  systemUnderTest: ValidationComposite
+  fieldValidationsSpy: FieldValidationSpy[]
+}
+
+const makeSystemUnderSystem = (): SystemUnderTest => {
+  const fieldValidationsSpy = [new FieldValidationSpy('any_field'), new FieldValidationSpy('any_field')]
+  const systemUnderTest = new ValidationComposite(fieldValidationsSpy)
+  return {
+    systemUnderTest,
+    fieldValidationsSpy
+  }
+}
+
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const fieldValidationSpy = new FieldValidationSpy('any_field')
-    fieldValidationSpy.error = new Error('first_error')
-    const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('second_error')
-    const systemUnderTest = new ValidationComposite([fieldValidationSpy, fieldValidationSpy2])
+    const { systemUnderTest, fieldValidationsSpy } = makeSystemUnderSystem()
+    fieldValidationsSpy[0].error = new Error('first_error')
+    fieldValidationsSpy[1].error = new Error('second_error')
     const error = systemUnderTest.validate('any_field', 'any_value')
     expect(error).toBe('first_error')
   })

0decf3802ece14aa84b5ad7464c3a9bc38f81cef
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-19 19:39:44 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -2,10 +2,12 @@ import faker from 'faker'
 import 'jest-localstorage-mock'
 import { LocalStorageAdapter } from './local-storage-adapter'
 
+const makeSystemUnderTest = (): LocalStorageAdapter => new LocalStorageAdapter()
+
 describe('LocalStorageAdapter', () => {
   beforeEach(() => localStorage.clear())
   test('Should call localStorage with correct values', async () => {
-    const systemUnderTest = new LocalStorageAdapter()
+    const systemUnderTest = makeSystemUnderTest()
     const key = faker.database.column()
     const value = faker.random.word()
     await systemUnderTest.set(key, value)

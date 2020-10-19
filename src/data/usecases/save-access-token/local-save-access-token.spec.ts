e9938046d57755bd5d432105c7d14dea2600b058
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-19 19:05:23 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -2,10 +2,23 @@ import { SetStorageSpy } from '@/data/test/mock-storage'
 import { LocalSaveAccessToken } from './local-save-access-token'
 import faker from 'faker'
 
+type SystemUnderTestTypes = {
+  systemUnderTest: LocalSaveAccessToken
+  setStorageSpy: SetStorageSpy
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const setStorageSpy = new SetStorageSpy()
+  const systemUnderTest = new LocalSaveAccessToken(setStorageSpy)
+  return {
+    systemUnderTest,
+    setStorageSpy
+  }
+}
+
 describe('LocalSaveAccessToken', () => {
   test('Should call SetStorage with correct value', async () => {
-    const setStorageSpy = new SetStorageSpy()
-    const systemUnderTest = new LocalSaveAccessToken(setStorageSpy)
+    const { systemUnderTest, setStorageSpy } = makeSystemUnderTest()
     const accessToken = faker.random.uuid()
     await systemUnderTest.save(accessToken)
     expect(setStorageSpy.key).toBe('accessToken')

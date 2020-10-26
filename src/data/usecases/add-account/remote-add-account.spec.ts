883215c06750a5ae8793f846b79c1e0bd3c75485
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-10-26 19:29:26 -0300

    test: ensure RemoteAddAccount return an AccountModel on success

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -1,7 +1,7 @@
 import { HttpPostClientSpy } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
 import { AddAccountParams } from '@/domain/usecases'
-import { mockAddAccountParams } from '@/domain/test'
+import { mockAccountModel, mockAddAccountParams } from '@/domain/test'
 import { AccountModel } from '@/domain/models'
 import { EmailInUseError, UnexpectedError } from '@/domain/errors'
 import { RemoteAddAccount } from './remote-add-account'
@@ -69,5 +69,16 @@ describe('RemoteAuthentication', () => {
     }
     const promise = systemUnderTest.add(mockAddAccountParams())
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
+    const httpResult = mockAccountModel()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+    const account = await systemUnderTest.add(mockAddAccountParams())
+    expect(account).toEqual(httpResult)
   })
 })

856735e549230a438c7843013e2cc0596b43aee6
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-03 21:21:29 -0300

    test: ensure Signup change name status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,28 +1,51 @@
-import { RenderResult, render } from '@testing-library/react'
 import React from 'react'
+import faker from 'faker'
+import { RenderResult, render, cleanup, fireEvent } from '@testing-library/react'
 import SignUp from './signup'
-import { Helper } from '@/presentation/test'
+import { Helper, ValidationStub } from '@/presentation/test'
 
 type SystemUnderTestTypes = {
   systemUnderTest: RenderResult
 }
 
-const makeSystemUnderTest = (): SystemUnderTestTypes => {
-  const systemUnderTest = render(<SignUp />)
+type SystemUnderTestParams = {
+  validationError: string
+}
+
+const makeSystemUnderTest = (params?: SystemUnderTestParams): SystemUnderTestTypes => {
+  const validationStub = new ValidationStub()
+  validationStub.errorMessage = params?.validationError || ''
+
+  const systemUnderTest = render(<SignUp validation={validationStub} />)
   return {
     systemUnderTest
   }
 }
 
+const populateField = (systemUnderTest: RenderResult, fieldName: string, value = faker.random.word()): void => {
+  const input = systemUnderTest.getByTestId(fieldName)
+  fireEvent.input(input, { target: { value } })
+}
+
 describe('Signup component', () => {
+  afterEach(cleanup)
+
   test('Should start with initial state', () => {
-    const { systemUnderTest } = makeSystemUnderTest()
     const validationError = 'Campo obrigatório'
+    const { systemUnderTest } = makeSystemUnderTest({ validationError })
     Helper.testChildCount(systemUnderTest, 'error-wrapper', 0)
     Helper.testButtonIsDisabled(systemUnderTest, 'submit', true)
     Helper.testStatusForField(systemUnderTest, 'name', validationError)
-    Helper.testStatusForField(systemUnderTest, 'email', validationError)
-    Helper.testStatusForField(systemUnderTest, 'password', validationError)
-    Helper.testStatusForField(systemUnderTest, 'passwordConfirmation', validationError)
+    Helper.testStatusForField(systemUnderTest, 'email', 'Campo obrigatório')
+    Helper.testStatusForField(systemUnderTest, 'password', 'Campo obrigatório')
+    Helper.testStatusForField(systemUnderTest, 'passwordConfirmation', 'Campo obrigatório')
+  })
+
+  test('Should show name error if Validation fails', () => {
+    const validationError = faker.random.words()
+    const { systemUnderTest } = makeSystemUnderTest({ validationError })
+
+    populateField(systemUnderTest, 'name')
+    Helper.testStatusForField(systemUnderTest, 'name', validationError)
   })
 })

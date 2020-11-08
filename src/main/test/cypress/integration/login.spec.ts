d2b3415a72ffe884995d6e5c223010ebe4461293
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-07 23:17:05 -0300

    test: ensure Login present error message if invalid credentials are provided

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,40 +1,47 @@
 import faker from 'faker'
 
+const baseUrl: string = Cypress.config().baseUrl
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
 
   it('Should load with correct initial state', () => {
-    cy.get('[data-testid="email"]').should('have.attr', 'readonly')
-    cy.get('[data-testid="password"]').should('have.attr', 'readonly')
-    cy.get('[data-testid="email-status"]')
-      .should('have.attr', 'title', 'Campo obrigatório')
-      .should('contain.text', '🔴')
-    cy.get('[data-testid="password-status"]')
-      .should('have.attr', 'title', 'Campo obrigatório')
-      .should('contain.text', '🔴')
-    cy.get('[data-testid="submit"]').should('have.attr', 'disabled', 'disabled')
-    cy.get('[data-testid="error-wrap"]').should('not.have.descendants')
+    cy.getByTestId('email').should('have.attr', 'readonly')
+    cy.getByTestId('password').should('have.attr', 'readonly')
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatório').should('contain.text', '🔴')
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Campo obrigatório').should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled', 'disabled')
+    cy.getByTestId('error-wrapper').should('not.have.descendants')
   })
 
   it('Should present error state if form is invalid', () => {
-    cy.get('[data-testid="email"]').focus().type(faker.random.word())
-    cy.get('[data-testid="email-status"]').should('have.attr', 'title', 'Valor inválido').should('contain.text', '🔴')
-    cy.get('[data-testid="password"]').focus().type(faker.random.alphaNumeric(3))
-    cy.get('[data-testid="password-status"]')
-      .should('have.attr', 'title', 'Valor inválido')
-      .should('contain.text', '🔴')
-    cy.get('[data-testid="submit"]').should('have.attr', 'disabled', 'disabled')
-    cy.get('[data-testid="error-wrap"]').should('not.have.descendants')
+    cy.getByTestId('email').focus().type(faker.random.word())
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Valor inválido').should('contain.text', '🔴')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Valor inválido').should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled', 'disabled')
+    cy.getByTestId('error-wrapper').should('not.have.descendants')
   })
 
   it('Should present valid state if form is valid', () => {
-    cy.get('[data-testid="email"]').focus().type(faker.internet.email())
-    cy.get('[data-testid="email-status"]').should('have.attr', 'title', 'Deu certo!').should('contain.text', '🔵')
-    cy.get('[data-testid="password"]').focus().type(faker.random.alphaNumeric(5))
-    cy.get('[data-testid="password-status"]').should('have.attr', 'title', 'Deu certo!').should('contain.text', '🔵')
-    cy.get('[data-testid="submit"]').should('not.have.attr', 'disabled', 'disabled')
-    cy.get('[data-testid="error-wrap"]').should('not.have.descendants')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Deu certo!').should('contain.text', '🔵')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Deu certo!').should('contain.text', '🔵')
+    cy.getByTestId('submit').should('not.have.attr', 'disabled', 'disabled')
+    cy.getByTestId('error-wrapper').should('not.have.descendants')
+  })
+
+  it('Should present error if invalid credentials are provided', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('submit').getByTestId('spinner').should('exist')
+    cy.getByTestId('error-wrapper').getByTestId('main-error').should('not.exist')
+    cy.getByTestId('submit').getByTestId('spinner').should('not.exist')
+    cy.getByTestId('error-wrapper').getByTestId('main-error').should('contain.text', 'Credenciais inválidas')
+    cy.url().should('equal', `${baseUrl}/login`)
   })
 })

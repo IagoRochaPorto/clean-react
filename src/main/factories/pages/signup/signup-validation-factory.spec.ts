f51fa100bf66ab37ea65b0f0a9aadd64a9564bf9
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-09 11:01:55 -0300

    feat: add minLength validation for input name in Signup

--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -7,7 +7,7 @@ describe('LoginValidationFactory', () => {
     const composite = makeSignupValidation()
     expect(composite).toEqual(
       ValidationComposite.build([
-        ...Builder.field('name').required().build(),
+        ...Builder.field('name').required().minLength(5).build(),
         ...Builder.field('email').required().email().build(),
         ...Builder.field('password').required().minLength(5).build(),
         ...Builder.field('passwordConfirmation').required().minLength(5).build()

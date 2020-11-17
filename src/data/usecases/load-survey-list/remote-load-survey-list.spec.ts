360195d24a13efc1810382588a6e70604350ad44
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-17 06:44:46 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.spec.ts
@@ -1,14 +1,17 @@
 import { RemoteLoadSurveyList } from './remote-load-survey-list'
 import { HttpGetClientSpy } from '@/data/test'
 import faker from 'faker'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { UnexpectedError } from '@/domain/errors'
+import { SurveyModel } from '@/domain/models'
 
 type SystemUnderTestTypes = {
   systemUnderTest: RemoteLoadSurveyList
-  httpGetClientSpy: HttpGetClientSpy
+  httpGetClientSpy: HttpGetClientSpy<SurveyModel[]>
 }
 
 const makeSystemUnderTest = (url = faker.internet.url()): SystemUnderTestTypes => {
-  const httpGetClientSpy = new HttpGetClientSpy()
+  const httpGetClientSpy = new HttpGetClientSpy<SurveyModel[]>()
   const systemUnderTest = new RemoteLoadSurveyList(url, httpGetClientSpy)
 
   return {
@@ -23,5 +26,14 @@ describe('RemoteLoadSurveyList', () => {
     const { systemUnderTest, httpGetClientSpy } = makeSystemUnderTest(url)
     await systemUnderTest.loadAll()
     expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  test('Should throw UnexpectedError if HttpPGetClient returns 403', async () => {
+    const { systemUnderTest, httpGetClientSpy } = makeSystemUnderTest()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = systemUnderTest.loadAll()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })

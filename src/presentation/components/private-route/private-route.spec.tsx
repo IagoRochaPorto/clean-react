3275d03575260cf18a3e69db9c3dec076bf48e40
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-28 11:46:19 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import PrivateRoute from './private-route'
+
+type SystemUnderTestTypes = {
+  history: MemoryHistory
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+
+  render(
+    <Router history={history}>
+      <PrivateRoute />
+    </Router>
+  )
+  return {
+    history
+  }
+}
 
 describe('PrivateRoute', () => {
   test('Should redirect to /login if token is empty', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-    render(
-      <Router history={history}>
-        <PrivateRoute />
-      </Router>
-    )
+    const { history } = makeSystemUnderTest()
     expect(history.location.pathname).toBe('/login')
   })
 })

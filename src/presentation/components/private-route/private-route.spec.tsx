3226164f61738efa90c1e07d01ee10e68df6c637
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-28 11:55:19 -0300

    test: ensure PrivateRoute renders component if token is not empty

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -3,27 +3,34 @@ import { render } from '@testing-library/react'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory, MemoryHistory } from 'history'
 import PrivateRoute from './private-route'
+import { ApiContext } from '@/presentation/contexts'
+import { mockAccountModel } from '@/domain/test'
 
 type SystemUnderTestTypes = {
   history: MemoryHistory
 }
 
-const makeSystemUnderTest = (): SystemUnderTestTypes => {
+const makeSystemUnderTest = (account = mockAccountModel()): SystemUnderTestTypes => {
   const history = createMemoryHistory({ initialEntries: ['/'] })
 
   render(
-    <Router history={history}>
-      <PrivateRoute />
-    </Router>
+    <ApiContext.Provider value={{ getCurrentAccount: () => account }}>
+      <Router history={history}>
+        <PrivateRoute />
+      </Router>
+    </ApiContext.Provider>
   )
-  return {
-    history
-  }
+  return { history }
 }
 
 describe('PrivateRoute', () => {
   test('Should redirect to /login if token is empty', () => {
-    const { history } = makeSystemUnderTest()
+    const { history } = makeSystemUnderTest(null)
     expect(history.location.pathname).toBe('/login')
+  })
+
+  test('Should render current component if token is not empty', () => {
+    const { history } = makeSystemUnderTest()
+    expect(history.location.pathname).toBe('/')
   })
 })

e21750257faa9e49e29612f542e7dc24911fc2d4
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-28 13:53:34 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
 
+const makeSystemUnderSystem = (): void => {
+  render(<SurveyList />)
+}
+
 describe('SurveyList Component', () => {
   test('Should present 4 empty items on start', () => {
-    render(<SurveyList />)
+    makeSystemUnderSystem()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
   })

e91d1d547b42e92d4335470734504cfa4f483304
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-29 10:26:19 -0300

    test: ensure AuthorizeHttpGetClientDecorator merge headers to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -52,4 +52,22 @@ describe('AuthorizeHttpGetClientDecorator', () => {
     expect(httpGetClientSpy.url).toBe(httpRequest.url)
     expect(httpGetClientSpy.headers).toEqual({ 'x-access-token': getStorageSpy.value.accessToken })
   })
+
+  test('Should merge headers to HttpGetClient', async () => {
+    const { systemUnderTest, httpGetClientSpy, getStorageSpy } = makeSystemUnderTest()
+    getStorageSpy.value = mockAccountModel()
+    const field = faker.random.words()
+    const httpRequest: HttpGetParams = {
+      url: faker.internet.url(),
+      headers: {
+        field
+      }
+    }
+    await systemUnderTest.get(httpRequest)
+    expect(httpGetClientSpy.url).toBe(httpRequest.url)
+    expect(httpGetClientSpy.headers).toEqual({
+      field,
+      'x-access-token': getStorageSpy.value.accessToken
+    })
+  })
 })

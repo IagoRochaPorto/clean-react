6e46efb18f9de51bc8f9107368ce7057783d3e2c
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-29 10:10:42 -0300

    test: ensure AuthorizeHttpGetClientDecorator not add token if getStorage is invalid

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -1,25 +1,43 @@
 import { AuthorizeHttpGetClientDecorator } from '@/main/decorators'
-import { mockGetRequest, GetStorageSpy } from '@/data/test'
+import { mockGetRequest, GetStorageSpy, HttpGetClientSpy } from '@/data/test'
+import faker from 'faker'
+import { HttpGetParams } from '@/data/protocols/http'
 
 type SystemUnderTestTypes = {
   systemUnderTest: AuthorizeHttpGetClientDecorator
   getStorageSpy: GetStorageSpy
+  httpGetClientSpy: HttpGetClientSpy
 }
 
 const makeSystemUnderTest = (): SystemUnderTestTypes => {
   const getStorageSpy = new GetStorageSpy()
-  const systemUnderTest = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const systemUnderTest = new AuthorizeHttpGetClientDecorator(getStorageSpy, httpGetClientSpy)
 
   return {
     systemUnderTest,
-    getStorageSpy
+    getStorageSpy,
+    httpGetClientSpy
   }
 }
 
 describe('AuthorizeHttpGetClientDecorator', () => {
-  test('Should call getStorage with correct value', () => {
+  test('Should call getStorage with correct value', async () => {
     const { getStorageSpy, systemUnderTest } = makeSystemUnderTest()
-    systemUnderTest.get(mockGetRequest())
+    await systemUnderTest.get(mockGetRequest())
     expect(getStorageSpy.key).toBe('account')
+  })
+
+  test('Should not add token if getStorage is invalid', async () => {
+    const { systemUnderTest, httpGetClientSpy } = makeSystemUnderTest()
+    const httpRequest: HttpGetParams = {
+      url: faker.internet.url(),
+      headers: {
+        field: faker.random.words()
+      }
+    }
+    await systemUnderTest.get(httpRequest)
+    expect(httpGetClientSpy.url).toBe(httpRequest.url)
+    expect(httpGetClientSpy.headers).toEqual(httpRequest.headers)
   })
 })

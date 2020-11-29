356c1bd355d9de088376ebfec2b51b265a2c4b1e
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-29 09:59:38 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -1,10 +1,24 @@
 import { AuthorizeHttpGetClientDecorator } from '@/main/decorators'
 import { mockGetRequest, GetStorageSpy } from '@/data/test'
 
+type SystemUnderTestTypes = {
+  systemUnderTest: AuthorizeHttpGetClientDecorator
+  getStorageSpy: GetStorageSpy
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  const getStorageSpy = new GetStorageSpy()
+  const systemUnderTest = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+
+  return {
+    systemUnderTest,
+    getStorageSpy
+  }
+}
+
 describe('AuthorizeHttpGetClientDecorator', () => {
   test('Should call getStorage with correct value', () => {
-    const getStorageSpy = new GetStorageSpy()
-    const systemUnderTest = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+    const { getStorageSpy, systemUnderTest } = makeSystemUnderTest()
     systemUnderTest.get(mockGetRequest())
     expect(getStorageSpy.key).toBe('account')
   })

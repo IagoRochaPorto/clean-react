00a16d218ee32515e20e22b2ba24c4f684f4d558
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-11-30 20:58:40 -0300

    refactor: remove unused code

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -20,10 +20,4 @@ describe('CurrentAccountAdapter', () => {
     expect(getSpy).toHaveBeenCalledWith('account')
     expect(result).toEqual(account)
   })
-
-  test('Should throw UnexpectedError', () => {
-    expect(() => {
-      setCurrentAccountAdapter(undefined)
-    }).toThrow(new UnexpectedError())
-  })
 })

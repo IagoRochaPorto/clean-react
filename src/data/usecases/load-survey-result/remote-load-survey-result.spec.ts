d369ce83a63ce4c67e20f5db17114d19655ffb77
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-12-06 19:32:49 -0300

    refactor: move SystemUnderTest creation to a factory method

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -2,11 +2,24 @@ import { RemoteLoadSurveyResult } from './remote-load-survey-result'
 import { HttpGetClientSpy } from '@/data/test'
 import faker from 'faker'
 
+type SystemUnderTestTypes = {
+  systemUnderTest: RemoteLoadSurveyResult
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSystemUnderTest = (url = faker.internet.url()): SystemUnderTestTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const systemUnderTest = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+  return {
+    systemUnderTest,
+    httpGetClientSpy
+  }
+}
+
 describe('RemoteLoadSurveyResult', () => {
   test('Should call HttpGetClient with correct url', async () => {
     const url = faker.internet.url()
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const systemUnderTest = new RemoteLoadSurveyResult(url, httpGetClientSpy)
+    const { systemUnderTest, httpGetClientSpy } = makeSystemUnderTest(url)
     await systemUnderTest.load()
     expect(httpGetClientSpy.url).toBe(url)
   })

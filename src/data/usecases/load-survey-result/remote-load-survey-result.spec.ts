d486f9a60151706afa7b6b0bf8ac00eceb93f4d5
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-12-06 19:40:29 -0300

    test: ensure RemoteLoadSurveyResult throws AccessDeniedError on 403

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,5 +1,7 @@
 import { RemoteLoadSurveyResult } from './remote-load-survey-result'
 import { HttpGetClientSpy } from '@/data/test'
+import { HttpStatusCode } from '@/data/protocols/http'
+import { AccessDeniedError } from '@/domain/errors'
 import faker from 'faker'
 
 type SystemUnderTestTypes = {
@@ -22,5 +24,14 @@ describe('RemoteLoadSurveyResult', () => {
     const { systemUnderTest, httpGetClientSpy } = makeSystemUnderTest(url)
     await systemUnderTest.load()
     expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  test('Should throw AccessDeniedError if HttpGetClient returns 403', async () => {
+    const { systemUnderTest, httpGetClientSpy } = makeSystemUnderTest()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = systemUnderTest.load()
+    await expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })

febb76e01ec0b85cadfb5a68cdf7fb15666cb3d9
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-12-22 10:39:29 -0300

    refactor: change SurveyResult highlight item

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -9,13 +9,23 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrapper}>
       <Header />
       <div className={Styles.contentWrapper}>
-        {false && (
+        {true && (
           <>
             <hgroup>
               <Calendar date={new Date()} className={Styles['calendar-wrapper']} />
               <h2>Lorem Ipsum dolor sit amet?</h2>
             </hgroup>
             <FlipMove className={Styles['answers-list']}>
+              <li>
+                <img src="https://fordevs.herokuapp.com/static/img/logo-react.png" />
+                <span className={Styles.answer}>LoremJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li className={Styles.active}>
+                <img src="https://fordevs.herokuapp.com/static/img/logo-react.png" />
+                <span className={Styles.answer}>LoremJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
               <li>
                 <img src="https://fordevs.herokuapp.com/static/img/logo-react.png" />
                 <span className={Styles.answer}>LoremJS</span>

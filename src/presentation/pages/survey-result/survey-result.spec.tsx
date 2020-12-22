170b336d354b35cfbe7554ebdf0393797e9d9c50
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-12-22 17:23:57 -0300

    test: ensure SurveyResult present correct result on success

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -2,14 +2,15 @@ import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
 import { SurveyResult } from '@/presentation/pages'
 import { ApiContext } from '@/presentation/contexts'
-import { mockAccountModel, LoadSurveyResultSpy } from '@/domain/test'
+import { mockAccountModel, LoadSurveyResultSpy, mockSurveyResultModel } from '@/domain/test'
 
 type SystemUnderTestTypes = {
   loadSurveyResultSpy: LoadSurveyResultSpy
 }
 
-const makeSystemUnderTest = (): SystemUnderTestTypes => {
+const makeSystemUnderTest = (surveyResult = mockSurveyResultModel()): SystemUnderTestTypes => {
   const loadSurveyResultSpy = new LoadSurveyResultSpy()
+  loadSurveyResultSpy.surveyResult = surveyResult
   render(
     <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
       <SurveyResult loadSurveyResult={loadSurveyResultSpy} />
@@ -34,5 +35,32 @@ describe('SurveyResult Component', () => {
     const { loadSurveyResultSpy } = makeSystemUnderTest()
     await waitFor(() => screen.getByTestId('survey-result'))
     expect(loadSurveyResultSpy.callsCount).toBe(1)
+  })
+
+  test('Should present SurveyResult data on success', async () => {
+    const surveyResult = Object.assign(mockSurveyResultModel(), {
+      date: new Date('2020-01-10T00:00:00')
+    })
+    makeSystemUnderTest(surveyResult)
+    await waitFor(() => screen.getByTestId('survey-result'))
+    expect(screen.getByTestId('day')).toHaveTextContent('10')
+    expect(screen.getByTestId('month')).toHaveTextContent('jan')
+    expect(screen.getByTestId('year')).toHaveTextContent('2020')
+    expect(screen.getByTestId('question')).toHaveTextContent(surveyResult.question)
+    expect(screen.getByTestId('answers').childElementCount).toBe(2)
+    const answerWrapper = screen.queryAllByTestId('answer-wrapper')
+    expect(answerWrapper[0]).toHaveClass('active')
+    expect(answerWrapper[1]).not.toHaveClass('active')
+
+    const images = screen.queryAllByTestId('image')
+    expect(images[0]).toHaveAttribute('src', surveyResult.answers[0].image)
+    expect(images[0]).toHaveAttribute('alt', surveyResult.answers[0].answer)
+    expect(images[1]).toBeFalsy()
+    const answers = screen.queryAllByTestId('answer')
+    expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer)
+    expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer)
+    const percents = screen.queryAllByTestId('percent')
+    expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`)
+    expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`)
   })
 })

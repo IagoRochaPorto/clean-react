156f51a173602178d9b8fe85b201f37b77965b6d
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-12-22 09:18:24 -0300

    refactor: move Calendar to its own Component

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -11,27 +11,19 @@ const makeSystemUnderTest = (survey = mockSurveyModel()): void => {
 describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: true,
-      date: new Date('2020-01-10T00:00:00')
+      didAnswer: true
     })
     makeSystemUnderTest(survey)
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
-    expect(screen.getByTestId('day')).toHaveTextContent('10')
-    expect(screen.getByTestId('month')).toHaveTextContent('jan')
-    expect(screen.getByTestId('year')).toHaveTextContent('2020')
   })
 
   test('Should render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: false,
-      date: new Date('2019-05-03T00:00:00')
+      didAnswer: false
     })
     makeSystemUnderTest(survey)
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
-    expect(screen.getByTestId('day')).toHaveTextContent('03')
-    expect(screen.getByTestId('month')).toHaveTextContent('mai')
-    expect(screen.getByTestId('year')).toHaveTextContent('2019')
   })
 })

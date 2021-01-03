8555e29b31791a2549713e2844bde48c88063680
Iago Rocha Porto <iagorochaporto@gmail.com>
2021-01-03 15:50:30 -0300

    refactor: remove unused imports

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useContext } from 'react'
 import Styles from './signup-styles.scss'
 import { LoginHeader, Footer } from '@/presentation/components'
 import { ApiContext } from '@/presentation/contexts'
